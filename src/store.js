5237940fb17f877e74a97115140befe6a9bee7f3
Sergiy <sergiy.shcherbanyuk@gmail.com>
2019-11-23 01:23:58 +0200

    localstorage saving

--- src/store.js
+++ src/store.js
@@ -1,13 +1,21 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducers/rootReducer'
 import thunk from 'redux-thunk'
+import { loadState, saveState } from './selectors/localStorage'
+
+const persistedState = loadState();
 
 export const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  )
+  {notesReducer: persistedState.notes, commentsReducer: persistedState.comments},
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+store.subscribe(() => {
+  saveState({
+    notes: store.getState().notesReducer,
+    comments: store.getState().commentsReducer
+  });
+});
 
 export default store;
